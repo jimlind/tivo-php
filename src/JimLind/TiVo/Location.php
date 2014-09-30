73d2fb89bcd4343376e2157fe6aabe9a7183ad3b
Jim Lind <jimlind@me.com>
2014-09-29 23:00:04 -0500

    Update Location to a Symfony2 Ruleset
    Also, move my about me location.

--- src/JimLind/TiVo/Location.php
+++ src/JimLind/TiVo/Location.php
@@ -5,17 +5,35 @@ namespace JimLind\TiVo;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Process\Process;
 
+/**
+ * Location is a service for finding a TiVo on your local network.
+ */
 class Location
 {
     protected $process = null;
     protected $logger = null;
 
-    function __construct(Process $process, LoggerInterface $logger) {
+    /**
+     * Constructor
+     * 
+     * @param \Symfony\Component\Process\Process $process The Symfony Process Component
+     * @param \Psr\Log\LoggerInterface           $logger  A PSR-0 Logger
+     */
+    public function __construct(Process $process, LoggerInterface $logger)
+    {
         $this->process = $process;
         $this->logger  = $logger;
     }
 
-    public function find() {
+    /**
+     * Attempt to find the TiVo and log any problems.
+     * 
+     * Returns IP address or false
+     * 
+     * @return boolean|string 
+     */
+    public function find()
+    {
         $avahiOutput = $this->fetchAvahi();
 
         if (empty($avahiOutput)) {
@@ -23,35 +41,53 @@ class Location
                 'Problem locating a proper device on the network. ' .
                 'The avahi-browse tool may not be installed. '
             );
+            // Exit early.
             return false;
         }
-
+        
         $ipMatch = $this->parseAvahi($avahiOutput);
         if ($ipMatch) {
             return $ipMatch;
         }
 
         $this->logger->warning('Unable to parse IP from Avahi.');
+        // TiVo not found.
         return false;
     }
 
-    protected function fetchAvahi() {
+    /**
+     * Execute the command line to run Avahi.
+     * 
+     * @return string
+     */
+    protected function fetchAvahi()
+    {
         // Command to find the records for the TiVo on TCP
         $command = 'avahi-browse -l -r -t _tivo-videos._tcp';
 
         $this->process->setCommandLine($command);
         $this->process->setTimeout(60); // 1 minute
         $this->process->run();
+
         return $this->process->getOutput();
     }
 
-    protected function parseAvahi($avahiOutput) {
+    /**
+     * Regular Expression to find IP in Avahi output.
+     * 
+     * @param string $avahiOutput
+     * 
+     * @return boolean|string
+     */
+    protected function parseAvahi($avahiOutput)
+    {
         $matches = array();
         $pattern = '/^\s+address = \[(\d+\.\d+\.\d+\.\d+)\]$/m';
         preg_match($pattern, $avahiOutput, $matches);
         if (!empty($matches) && isset($matches[1])) {
             return $matches[1];
         }
+
         return false;
     }
 }
