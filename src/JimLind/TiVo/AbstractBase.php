3bd6633961d80d180a377b3afcb71fa1a2caa74e
Jim Lind <spoon.vw@gmail.com>
2015-09-07 22:26:08 -0500

    Codesniffed
    Update PR’ed code to match the previous code sniffs

--- src/JimLind/TiVo/AbstractBase.php
+++ src/JimLind/TiVo/AbstractBase.php
@@ -5,13 +5,18 @@ namespace JimLind\TiVo;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 
+/**
+ * Base class for setting up logger methods and default logger
+ */
 abstract class AbstractBase
 {
     use LoggerAwareTrait;
 
+    /**
+     * No parameters neccessary
+     */
     public function __construct()
     {
         $this->logger = new NullLogger();
     }
-
 }
